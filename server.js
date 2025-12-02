b7344fb6e047c977e308565b51a4ebf87284dfe7
Mikidiasc <matheusdiasc33@gmail.com>
2025-12-01 22:00:22 -0300

    ajustando server.js v2

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-
 const express = require('express');
 const path = require('path');
+
+const app = express();
 
 const PORT = process.env.PORT ? Number(process.env.PORT) : 3000;
 const PUBLIC_DIR = path.join(__dirname, 'public');
@@ -8,14 +9,14 @@ const PUBLIC_DIR = path.join(__dirname, 'public');
 app.use(express.static(PUBLIC_DIR));
 
 const handleRoot = (req, res) => {
-    res.sendFile(path.join(PUBLIC_DIR, 'index.html'));
-    console.log('Served /');
+  res.sendFile(path.join(PUBLIC_DIR, 'index.html'));
+  console.log('Served /');
 };
 
 app.get('/', handleRoot);
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
 
 const f1 = () => true;
